8a7d9c1a3668cc64dfe20287cf318acb1e07bce1
kamiladss <kdoskozha@gmail.com>
2024-01-28 23:01:15 +0600

    Lab2

---
+++ lab2/1exercises/ifelse-exer/exr5.py
@@ -0,0 +1,7 @@
+a=2
+b=2
+c=3
+d=3
+#for fix error about variables
+if a == b and c == d:
+  print("Hello")
---
+++ lab2/1exercises/ifelse-exer/exr6.py
@@ -0,0 +1,7 @@
+a=2
+b=2
+c=3
+d=4
+#for fix error about variables
+if a == b or c == d:
+  print("Hello")
